de40f9db401ca4d75514c626e8bb774ed2884616
Rob van Oostenrijk <robvanoostenrijk@users.noreply.github.com>
2024-10-23 06:14:18 +0400

    Workflow readme

--- extract-artifacts.sh
+++ extract-artifacts.sh
@@ -8,8 +8,7 @@ echo "[i] Clean dist folder"
 rm -f -R ./dist
 mkdir -p ./dist
 
-#for PLATFORM in linux/amd64 linux/arm64
-for PLATFORM in linux/arm64
+for PLATFORM in linux/amd64 linux/arm64
 do
     CONTAINER=$(docker create --platform ${PLATFORM} "${IMAGE}:${VERSION}")
     echo "[i] Created container ${CONTAINER:0:12}"
@@ -25,5 +24,4 @@ do
 done
 
 echo "[i] Grab version information"
-docker pull --platform linux/amd64 "${IMAGE}:${VERSION}"
 docker run --platform linux/amd64 --rm -i --log-driver=none -a stdin -a stdout -a stderr --entrypoint "/usr/sbin/nginx" "${IMAGE}:${VERSION}" -v 2> ./dist/version.txt
