65a0a37b21a330416b7bf717c8cff5f14f8f7be2
Rob van Oostenrijk <robvanoostenrijk@users.noreply.github.com>
2026-05-21 14:28:10 +0400

    Update to use Github native runners

--- scripts/extract-artifacts.sh
+++ scripts/extract-artifacts.sh
@@ -3,26 +3,23 @@
 IMAGE=$1
 VERSION=$2
 LIBRARY=$3
+PLATFORM=${4:-linux/amd64}
 
 echo "[i] Clean dist folder"
 rm -f -R ./dist
 mkdir -p ./dist
 
-for PLATFORM in linux/amd64 linux/arm64
-do
-    CONTAINER=$(docker create --platform ${PLATFORM} "${IMAGE}:${VERSION}")
-    echo "[i] Created container ${CONTAINER:0:12}"
+CONTAINER=$(docker create --platform ${PLATFORM} "${IMAGE}:${VERSION}")
+echo "[i] Created container ${CONTAINER:0:12}"
 
-    echo "[i] Extract assets"
-    docker cp "${CONTAINER}:/usr/sbin/nginx" ./dist/nginx
+echo "[i] Extract assets"
+docker cp "${CONTAINER}:/usr/sbin/nginx" ./dist/nginx
 
-    echo "[i] Create distribution archive"
-    XZ_OPT=-9 tar -C ./dist -Jcvf ./dist/nginx-http3-${LIBRARY}-${PLATFORM/\//-}.tar.xz nginx
+echo "[i] Create distribution archive"
+XZ_OPT=-9 tar -C ./dist -Jcvf ./dist/nginx-http3-${LIBRARY}-${PLATFORM/\//-}.tar.xz nginx
 
-    echo "[i] Removing container ${CONTAINER:0:12}"
-    docker rm $CONTAINER
-done
+echo "[i] Removing container ${CONTAINER:0:12}"
+docker rm $CONTAINER
 
 echo "[i] Grab version information"
-docker pull --platform linux/amd64 "${IMAGE}:${VERSION}"
-docker run --platform linux/amd64 --rm -i --log-driver=none -a stdin -a stdout -a stderr --entrypoint "/usr/sbin/nginx" "${IMAGE}:${VERSION}" -v 2> ./dist/version.txt
+docker run --platform ${PLATFORM} --rm -i --log-driver=none -a stdin -a stdout -a stderr --entrypoint "/usr/sbin/nginx" "${IMAGE}:${VERSION}" -v 2> ./dist/version.txt
